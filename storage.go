4b671e4a656ad0898eb3738ebef874dbaacf8b90
DavidWolf88 <bazdapm@gmail.com>
2023-03-02 09:20:41 +0100

    gobank

---
+++ storage.go
@@ -0,0 +1,16 @@
+package main
+
+import "database/sql"
+
+type Storage interface {
+	CreateAccount(*Account) error
+	DeleteAccount(int) error
+	UpdateAccount(*Account) error
+	GetAccountByID(int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error)
